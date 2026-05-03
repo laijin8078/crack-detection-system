cbde66b72f51f98c8f1a1d26217df5684427d569
gaohui <laijin8078@gmail.com>
2026-05-03 17:31:55 +0800

    完成cifar10

--- py_test.py
+++ py_test.py
@@ -5,14 +5,15 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.nn.functional as F
+import torch.nn as nn
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 )
-
-
 
 
 trainset = torchvision.datasets.CIFAR10(root='./data1', train=True,download=False, transform=transform)
@@ -36,3 +37,27 @@ imshow(torchvision.utils.make_grid(images))
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck')
 print(" ".join('%5s' % classes[labels[j]] for j in range(4)))
+
+#构建网络
+class CNNNet(nn.Module):
+    def __init__(self):
+        super(CNNNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=36, kernel_size=3, stride=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(1296, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(-1, 36*6*6)
+        x = F.relu(self.fc2(F.relu(self.fc1(x))))
+        return x
+
+net = CNNNet()
+net = net.to(device)
+
+#查看网络结构
+print(net)
