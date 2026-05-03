9197fe723f0fe91f219f42989f1b39d2caf103a8
gaohui <laijin8078@gmail.com>
2026-05-03 16:29:05 +0800

    完成cifar10

--- py_test.py
+++ py_test.py
@@ -1,19 +1,38 @@
+import cifar10
 import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == '__main__':
-    print("Support CUDA?:", torch.cuda.is_available())
-    x=torch.tensor([10.0])
-    x=x.cpu()
-    print(x)
 
-    y=torch.randn(3,3)
-    y=y.cpu()
-    print(y)
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
 
-    z=x+y
-    print(z)
 
-    from torch.backends import cudnn
-    print("Support cudnn?:",cudnn.is_acceptable(x))
 
-    #哈哈哈
+
+trainset = torchvision.datasets.CIFAR10(root='./data1', train=True,download=False, transform=transform)
+trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=0)
+testset = torchvision.datasets.CIFAR10(root='./data1', train=False,download=False, transform=transform)
+testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=0)
+
+#随机获取部分数据
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+#显示数据
+imshow(torchvision.utils.make_grid(images))
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck')
+print(" ".join('%5s' % classes[labels[j]] for j in range(4)))
